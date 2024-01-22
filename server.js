013693dab7b7b4626dc1238305f78fcb3d41c1e9
Jerry Nguyen Dinh <jdinh14@Administrators-MacBook-Pro.local>
2024-01-22 16:36:00 -0500

    all backend files

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express()
+app.use(express.json());
+app.use(cors())
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'reagents',
+    user: 'root',
+    password: 'password'
+})
+
+app.get('/products', (req, res) => {
+    const sql = 'SELECT * FROM productData';
+    connection.query(sql, (err, data) => {
+        if(err) throw err;
+        return res.json(data)
+    })
+})
+
+app.post('/subscriptions', (req, res) => {
+    const { email } = req.body;
+    const sql = 'INSERT INTO user_emails (email) VALUES (?)';
+    connection.query(sql, [email], (err, results) => {
+        if (err) {
+            return res.status(500).send('Error saving email');
+        }
+        res.status(200).send('Email saved successfully');
+    });
+});
+
+app.post('/unsubscribe', (req, res) => {
+    const { email } = req.body;
+    const sql = 'DELETE FROM user_emails WHERE email = ?';
+    connection.query(sql, [email], (err, results) => {
+        if (err) {
+            return res.status(500).send('Error deleting email');
+        }
+        res.status(200).send('Email removed successfully');
+    });
+});
+
+
+
+
+
+app.get('/', (req, res) => {
+    return res.json('Loading Backend...')
+})
+
+
+app.get("*", (req, res) => {
+    res.render('error', {
+        title: 'ERROR',
+        error: `404 Data Not Found`
+    })
+})
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000');
+})
